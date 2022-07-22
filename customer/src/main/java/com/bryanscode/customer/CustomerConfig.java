ad08c177bc3e60ac09115b74cf78f5a4bdb33119
jamalbryan22 <jamal_bryan@knights.ucf.edu>
2022-07-22 08:51:20 -0400

    Adding eureka-server microservice

--- customer/src/main/java/com/bryanscode/customer/CustomerConfig.java
+++ customer/src/main/java/com/bryanscode/customer/CustomerConfig.java
@@ -2,12 +2,15 @@ package com.bryanscode.customer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestTemplate;import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+
 
 @Configuration
 public class CustomerConfig {
 
   @Bean
+  @LoadBalanced
   public RestTemplate restTemplate() {
     return new RestTemplate();
   }
